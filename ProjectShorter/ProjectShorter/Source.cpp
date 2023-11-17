fa87ba08b7cf1b73454152f887e70067624b39c2
d0zlove <105043807+d0zlove@users.noreply.github.com>
2023-11-17 18:59:43 +0500

    Add files via upload

--- ProjectShorter/ProjectShorter/Source.cpp
+++ ProjectShorter/ProjectShorter/Source.cpp
@@ -6,95 +6,116 @@
 
 extern "C" {
 
-    // Функция для создания базы данных SQLite и таблицы "data"
-    void createDatabase(const char* dbName) {
-        sqlite3* db;
-        int result = sqlite3_open(dbName, &db);
-        if (result != SQLITE_OK) {
-            std::cerr << "Ошибка при создании базы данных: " << sqlite3_errmsg(db) << std::endl;
-            sqlite3_close(db);
-            return;
-        }
-        std::string query = "CREATE TABLE IF NOT EXISTS data (key TEXT PRIMARY KEY, data TEXT)";
-        result = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
-        if (result != SQLITE_OK) {
-            std::cerr << "Ошибка при создании таблицы: " << sqlite3_errmsg(db) << std::endl;
-        }
-        else {
-            std::cout << "Таблица успешно создана." << std::endl;
-        }
-        sqlite3_close(db);
-    }
+	// Функция для создания базы данных SQLite и таблицы "data"
+	void createDatabase(const char* dbName) {
+		sqlite3* db;
+		int result = sqlite3_open(dbName, &db);
+		if (result != SQLITE_OK) {
+			std::cerr << "Ошибка при создании базы данных: " << sqlite3_errmsg(db) << std::endl;
+			sqlite3_close(db);
+			return;
+		}
+		std::string query = "CREATE TABLE IF NOT EXISTS data (key TEXT PRIMARY KEY, data TEXT)";
+		result = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
+		if (result != SQLITE_OK) {
+			std::cerr << "Ошибка при создании таблицы: " << sqlite3_errmsg(db) << std::endl;
+		}
+		else {
+			std::cout << "Таблица успешно создана." << std::endl;
+		}
+		sqlite3_close(db);
+	}
 
-    // Функция для добавления данных в базу данных по ключу
-    void addData(const char* dbName, const char* key, const char* data) {
-        sqlite3* db;
-        int result = sqlite3_open(dbName, &db);
-        if (result != SQLITE_OK) {
-            std::cerr << "Ошибка при открытии базы данных: " << sqlite3_errmsg(db) << std::endl;
-            sqlite3_close(db);
-            return;
-        }
-        std::string query = "INSERT INTO data (key, data) VALUES ('" + std::string(key) + "', '" + std::string(data) + "')";
-        result = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
-        if (result != SQLITE_OK) {
-            std::cerr << "Ошибка при добавлении данных: " << sqlite3_errmsg(db) << std::endl;
-        }
-        else {
-            std::cout << "Данные успешно добавлены." << std::endl;
-        }
-        sqlite3_close(db);
-    }
+	// Функция для добавления данных в базу данных по ключу
+	void addData(const char* dbName, const char* key, const char* data) {
+		sqlite3* db;
+		int result = sqlite3_open(dbName, &db);
+		if (result != SQLITE_OK) {
+			std::cerr << "Ошибка при открытии базы данных: " << sqlite3_errmsg(db) << std::endl;
+			sqlite3_close(db);
+			return;
+		}
+		std::string query = "INSERT INTO data (key, data) VALUES ('" + std::string(key) + "', '" + std::string(data) + "')";
+		result = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
+		if (result != SQLITE_OK) {
+			std::cerr << "Ошибка при добавлении данных: " << sqlite3_errmsg(db) << std::endl;
+		}
+		else {
+			std::cout << "Данные успешно добавлены." << std::endl;
+		}
+		sqlite3_close(db);
+	}
 
-    // Функция для получения данных из базы данных по ключу
-    const char* getData(const char* dbName, const char* key) {
-        sqlite3* db;
-        int result = sqlite3_open(dbName, &db);
-        if (result != SQLITE_OK) {
-            std::cerr << "Ошибка при открытии базы данных: " << sqlite3_errmsg(db) << std::endl;
-            sqlite3_close(db);
-            return nullptr;
-        }
-        std::string query = "SELECT data FROM data WHERE key = '" + std::string(key) + "'";
-        sqlite3_stmt* stmt;
-        result = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr);
-        if (result != SQLITE_OK) {
-            std::cerr << "Ошибка при выполнении запроса: " << sqlite3_errmsg(db) << std::endl;
-            sqlite3_finalize(stmt);
-            sqlite3_close(db);
-            return nullptr;
-        }
-        const char* data = nullptr;
-        if (sqlite3_step(stmt) == SQLITE_ROW) {
-            data = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
-        }
-        sqlite3_finalize(stmt);
-        sqlite3_close(db);
-        return data;
-    }
+	// Функция для выполнения запроса и получения результата
+	static int callback(void* data, int argc, char** argv, char** azColName) {
+		std::string* result = static_cast<std::string*>(data);
+		for (int i = 0; i < argc; i++) {
+			*result = argv[i];
+		}
+		return 0;
+	}
 
-    // Функция для удаления данных из базы данных по ключу
-    void deleteData(const char* dbName, const char* key) {
-        sqlite3* db;
-        int result = sqlite3_open(dbName, &db);
-        if (result != SQLITE_OK) {
-            std::cerr << "Ошибка при открытии базы данных: " << sqlite3_errmsg(db) << std::endl;
-            sqlite3_close(db);
-            return;
-        }
-        std::string query = "DELETE FROM data WHERE key = '" + std::string(key) + "'";
-        result = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
-        if (result != SQLITE_OK) {
-            std::cerr << "Ошибка при удалении данных: " << sqlite3_errmsg(db) << std::endl;
-        }
-        else {
-            std::cout << "Данные успешно удалены." << std::endl;
-        }
-        sqlite3_close(db);
-    }
+	// Функция для получения данных по ключу из базы данных SQLite
+	__declspec(dllexport) const char* getDataFromDatabase(const char* key) {
+		sqlite3* db;
+		char* zErrMsg = 0;
+		int rc;
+		std::string result;
+
+		// Открываем соединение с базой данных
+		rc = sqlite3_open("database.db", &db);
+		if (rc) {
+			std::cerr << "Ошибка при открытии базы данных: " << sqlite3_errmsg(db) << std::endl;
+			return nullptr;
+		}
+
+		// Формируем SQL-запрос для получения данных по ключу
+		std::string sqlQuery = "SELECT data FROM data WHERE key = '" + std::string(key) + "';";
+
+		// Выполняем SQL-запрос
+		rc = sqlite3_exec(db, sqlQuery.c_str(), callback, &result, &zErrMsg);
+		if (rc != SQLITE_OK) {
+			std::cerr << "Ошибка при выполнении запроса: " << zErrMsg << std::endl;
+			sqlite3_free(zErrMsg);
+		}
+
+		// Закрываем соединение с базой данных
+		sqlite3_close(db);
+
+		const char* Readyresult = const_cast<char*>(result.c_str());
+		size_t lenС = std::strlen(Readyresult);
+		char* resultmem = new char[lenС + 1]; 
+		strcpy_s(resultmem, lenС + 1, Readyresult);
+
+		// Возвращаем результат в типе const char*
+
+		return resultmem;
+	}
 
 
-	__declspec(dllexport) const char* shorten_url(int URLOrID, const char* longURl) {
+
+	// Функция для удаления данных из базы данных по ключу
+	void deleteData(const char* dbName, const char* key) {
+		sqlite3* db;
+		int result = sqlite3_open(dbName, &db);
+		if (result != SQLITE_OK) {
+			std::cerr << "Ошибка при открытии базы данных: " << sqlite3_errmsg(db) << std::endl;
+			sqlite3_close(db);
+			return;
+		}
+		std::string query = "DELETE FROM data WHERE key = '" + std::string(key) + "'";
+		result = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
+		if (result != SQLITE_OK) {
+			std::cerr << "Ошибка при удалении данных: " << sqlite3_errmsg(db) << std::endl;
+		}
+		else {
+			std::cout << "Данные успешно удалены." << std::endl;
+		}
+		sqlite3_close(db);
+	}
+
+
+	__declspec(dllexport) const char* shorten_url(const char* longURl, int a) {
 
 		std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
@@ -106,11 +127,13 @@ extern "C" {
 			short_id += characters[random_index];
 		}
 
+		const char* userURL = const_cast<char*>(longURl);
 		const char* link = const_cast<char*>("http://127.0.0.1:5000/");//основная строка
 		const char* ID = const_cast<char*>(short_id.c_str()); //дополнительная строка
 
 		size_t lenA = std::strlen(link);// длина строки с ссылкой 
 		size_t lenB = std::strlen(ID);// длина строки с айди
+		size_t lenС = std::strlen(userURL);// длина строки с юзерским юрлом
 
 
 		char* shortURL = new char[lenA + lenB + 1]; // память под новую строку
@@ -120,23 +143,23 @@ extern "C" {
 		char* IDresult = new char[lenB + 1]; // память под ID
 		strcpy_s(IDresult, lenB + 1, ID);// копиируем ID
 
+		char* URLresult = new char[lenС + 1]; // память под ID
+		strcpy_s(URLresult, lenС + 1, userURL);// копиируем ID
+
 		const char* dbName = "database.db";
 		createDatabase(dbName);
 		addData(dbName, shortURL, longURl);
+		std::cerr << "Данные найдены: " << getDataFromDatabase(shortURL) << std::endl;
 
-        /*
+		/*
 		const char* data = getData(dbName, "key1");
 		if (data != nullptr) {
 			std::cout << "Полученные данные: " << data << std::endl;
 		}
-      
+
 		deleteData(dbName, "key2");*/
 
-		if (URLOrID == 1)
-			return shortURL;
-		else if (URLOrID == 0)
-			return IDresult;
 
-		return 0;
+		return shortURL;
 	}
 }
--- Projectshorterpy/main.py
+++ Projectshorterpy/main.py
@@ -1,16 +1,16 @@
 import ctypes
 
 # Загрузка библиотеки C++
-dll = ctypes.cdll.LoadLibrary("./ProjectShorter.dll")
+dll = ctypes.cdll.LoadLibrary("C:/Users/Админ/Desktop/ProjectShorter/x64/Debug/ProjectShorter.dll")
 
-def url(arg, longURl):
+def url(longURl):
     # Определение типа возвращаемого значения для функции библиотеки C++ (const char*)
-    dll.shorten_url.argtypes = ctypes.c_int,ctypes.c_wchar_p
+    dll.shorten_url.argtypes = [ctypes.c_char_p]
     dll.shorten_url.restype = ctypes.c_char_p
 
 
     # Вызов функции из библиотеки C++ и получение результата в Python
-    short_url_bytes = dll.shorten_url(arg,longURl)
+    short_url_bytes = dll.shorten_url(longURl.encode())
     short_url_str = short_url_bytes.decode("utf-8")
 
     return short_url_str
@@ -28,8 +28,24 @@ def index():
 @app.route('/result', methods=['POST'])
 def result():
     user_value = request.form['value']
-    processed_value = url(1,user_value) 
+    if len(user_value) > 1:
+        processed_value = url(user_value) 
+    else:
+        return render_template('index.html', processed_value = "Вы не указали ссылку")
+    
     return render_template('index.html', processed_value=processed_value,user_value=user_value)
+
+from flask import Flask, redirect
+@app.route('/<short_id>', methods=['GET'])
+def redirect_url(short_id):
+    dll.getDataFromDatabase.argtypes = [ctypes.c_char_p]
+    dll.getDataFromDatabase.restype = ctypes.c_char_p
+    
+    # Вызов функции из библиотеки C++ и получение результата в Python
+    short_url_bytes = dll.getDataFromDatabase(("http://127.0.0.1:5000/" + short_id).encode())
+    short_url_str = short_url_bytes.decode("utf-8")
+    
+    return redirect(short_url_str, code=302)
 
 if __name__ == '__main__':
     app.run()
