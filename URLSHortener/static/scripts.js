c69f1601e32213490a049dbefb1bad1fecbf058b
d0zlove <105043807+d0zlove@users.noreply.github.com>
2024-01-10 19:11:36 +0300

    Delete URLSHortener directory

--- URLSHortener/static/scripts.js
+++
@@ -1,8 +0,0 @@
-function p_check(){
-    var element = document.querySelector('.forma p');
-    var userURL = element.textContent.trim();
-
-    if (userURL.length > 30) {
-     element.textContent = "User URL: " + userURL.substring(0, 30) + "...";
-    }
-}
